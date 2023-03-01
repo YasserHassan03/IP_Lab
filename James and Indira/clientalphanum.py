cb064f6265e9b78faa63ee6d2b47eaf86bc98d09
J3T21 <jameset701@gmail.com>
2023-03-01 13:55:46 +0000

    working code to send accel vals to server (watch out ofr ip address) also using csv need to change to json

---
+++ James and Indira/clientalphanum.py
@@ -0,0 +1,21 @@
+import socket
+import csv
+import keyboard
+##print("in client...")
+#server name and port
+server_name='44.204.141.113'
+server_port=12000
+client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#set TCP connection
+#connection_socket assigned to client on server side
+client_socket.connect((server_name,server_port))
+
+with open ('C:/Users/james/Desktop/Imperial/Informationprocessing/Lab3/task1/software/pythonaccel/accelerometer.csv') as dat:
+    next(dat)
+    read=csv.reader(dat,delimiter=',')  
+    while (True):
+        for row in read:
+            print(row)
+            client_socket.send(','.join(row).encode())
+
+client_socket.close()
