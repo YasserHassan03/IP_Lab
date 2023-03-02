3e61ab6a123489e94a65381bb68013d1c9890d7c
aaashah <aisha@xshah.com>
2023-03-02 12:03:40 +0000

    data from csv file

--- Aisha And Hana/smoothness_score.py
+++ Aisha And Hana/smoothness_score.py
@@ -1,26 +1,34 @@
-def smoothness_score(acceleration_data):
+import csv
+import accelerometer.csv
 
-    jerk_list = []
+with open("accelerometer.csv", 'r') as file:
+  csvreader = csv.reader(file, delimiter=',')
 
-    
-    for i in range(1, len(acceleration_data)):
-        jerk = (acceleration_data[i] - acceleration_data[i-1]) / 0.1 #replace 0.1 with their value
-        jerk_list.append(jerk)
+for row in csvreader:
 
-    
-    jerk_magnitudes = []
+    def smoothness_score(row):
 
-    
-    for jerk in jerk_list:
-        jerk_magnitude = abs(jerk)
-        jerk_magnitudes.append(jerk_magnitude)
+        jerk_list = []
 
-    
-    average_jerk_magnitude = sum(jerk_magnitudes) / len(jerk_magnitudes)
+        
+        for i in range(1, len(row)):
+            jerk = (row[i] - accelerometer.csv[i-1]) / 0.1 #replace 0.1 with their value
+            jerk_list.append(jerk)
 
-    
-    smoothness_score = 1 / average_jerk_magnitude
-    max_smoothness_score = 1 / 0.1  # The maximum possible jerk magnitude is 0.1 m/s^2, assuming a perfectly smooth ride, #replace 0.1 with their value
-    normalised_smoothness_score = smoothness_score / max_smoothness_score
+        
+        jerk_magnitudes = []
 
-    return normalised_smoothness_score
+        
+        for jerk in jerk_list:
+            jerk_magnitude = abs(jerk)
+            jerk_magnitudes.append(jerk_magnitude)
+
+        
+        average_jerk_magnitude = sum(jerk_magnitudes) / len(jerk_magnitudes)
+
+        
+        smoothness_score = 1 / average_jerk_magnitude
+        max_smoothness_score = 1 / 0.1  # The maximum possible jerk magnitude is 0.1 m/s^2, assuming a perfectly smooth ride, #replace 0.1 with their value
+        normalised_smoothness_score = smoothness_score / max_smoothness_score
+
+        return normalised_smoothness_score
