fa39508b705e5ec0cb74a1873002374927f29492
YasserHassan03 <116260803+YasserHassan03@users.noreply.github.com>
2023-03-20 21:01:23 +0000

    seperate pages on website for david and rob

--- Aisha And Hana/cssing.py
+++ Aisha And Hana/cssing.py
@@ -77,44 +77,152 @@ def bob():
 
     return str(Markup(css + table_html))
 
-@app.route('/formatting/<DriverId>')
-def query_and_project_movies(DriverId, dynamodb=None):
-    if DriverId == 'Robert':
-        DriverId = 'Rob'
 
-    if not dynamodb:
-        dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+#def query_and_project_movies(DriverId, dynamodb=None):
+  #  if DriverId == 'Robert':
+   #     DriverId = 'Robert'
 
-    table = dynamodb.Table(DriverId+'sresults')
-    print(f"Get year, title, genres, and lead actor")
-    if DriverId == 'Rob':
-        DriverId = 'Robert'
-    response = table.query(
-        ProjectionExpression="#DriverId, JourneyId, smoothness_score",
-        ExpressionAttributeNames={"#DriverId": DriverId},
-        KeyConditionExpression=
-            Key('DriverId').eq(DriverId)
-    )
-    return response['Items']
+#    if not dynamodb:
+ #       dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
-# @app.route('/formatting/robdb')
+    #table = dynamodb.Table(DriverId+'sresults')
 
-# def query_and_project_movies2(DriverId, dynamodb=None):
-#     if not dynamodb:
-#         dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+#    print(f"Get year, title, genres, and lead actor")
+   # if DriverId == 'David':
+    #    DriverId = 'David'
+    #table = dynamodb.Table(DriverId+'sresults')
+    #response = table.query(
+     #   ProjectionExpression="#DriverId, JourneyId, smoothness_score",
+       # ExpressionAttributeNames={"#DriverId": DriverId},
+        #KeyConditionExpression=
+         #   Key('DriverId').eq(DriverId)
+    #)
+    #data = response['Items']
+    #bj = pd.DataFrame(json.loads(data))
+    #obj = obj.sort_values(by=['smoothness'], ascending=False)
+    #return obj
 
-#     table = dynamodb.Table('Davidsresults')
-#     print(f"Get year, title, genres, and lead actor")
+def query_david():
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('Davidsresults')
 
-#     response = table.query(
-#         ProjectionExpression="#DriverId, JourneyId, smoothness_score",
-#         ExpressionAttributeNames={"#DriverId": "DriverId"},
-#         KeyConditionExpression=
-#             Key('DriverId').eq(DriverId)
-#     )
-#     return response['Items']
+    response = table.scan()
+    data = response['Items']
+
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items']) 
+
+    obj = pd.DataFrame(json.loads(data))
+    #obj = obj.sort_values(by=['smoothness'], ascending=False)
+    return obj
+
+def query_robert():
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('Robsresults')
+
+    response = table.scan()
+    data = response['Items']
+
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items']) 
+
+    obj = pd.DataFrame(json.loads(data))
+    #obj = obj.sort_values(by=['smoothness'], ascending=False)
+    return obj
 
 
+@app.route('/formatting/David')
+def Fred():
+    # generate the HTML table string
+    obj = query_david()
+    table_html = obj.to_html(header="true", table_id="table", classes="table table-striped table-hover", 
+                               border="0", justify="center",
+                               
+                               render_links=True)
+                     
+                    
+    css = '''
+        <style>
+            table {
+                border-collapse: collapse !important;
+                width: 100% !important;
+                font-family: Arial, sans-serif !important;
+                font-size: 14px !important;
+                margin-bottom: 20px !important;
+            }
+        
+            th, td {
+                text-align: left !important;
+                padding: 8px !important;
+                border-bottom: 1px solid #ddd !important;
+                background-color: #ffffff
+            }
+        
+            th {
+                background-color: #b56bdd !important;
+            }
+        
+            tr:hover {
+                background-color: #b56bdd !important;
+            }
+            body {
+                background-color: #6bbfde;
+            }
+        </style>
+        '''
+    
+    
+    table_html = ftable_html = f"<html><head><meta http-equiv='refresh' content='10'><title>David's Results</title></head><body><h1 style='text-align:center;'>David's result</h1>{table_html}<p align=\"center\"></p></html>"
+
+    return str(Markup(css + table_html))
+
+@app.route('/formatting/Robert')
+def dave():
+    # generate the HTML table string
+    obj = query_robert()
+    table_html = obj.to_html(header="true", table_id="table", classes="table table-striped table-hover", 
+                               border="0", justify="center",
+                               
+                               render_links=True)
+                     
+                    
+    css = '''
+        <style>
+            table {
+                border-collapse: collapse !important;
+                width: 100% !important;
+                font-family: Arial, sans-serif !important;
+                font-size: 14px !important;
+                margin-bottom: 20px !important;
+            }
+        
+            th, td {
+                text-align: left !important;
+                padding: 8px !important;
+                border-bottom: 1px solid #ddd !important;
+                background-color: #ffffff
+            }
+        
+            th {
+                background-color: #b56bdd !important;
+            }
+        
+            tr:hover {
+                background-color: #b56bdd !important;
+            }
+            body {
+                background-color: #6bbfde;
+            }
+        </style>
+        '''
+    
+    
+    table_html = ftable_html = f"<html><head><meta http-equiv='refresh' content='10'><title>Rob's Leaderboard</title></head><body><h1 style='text-align:center;'>Rob's Leaderboard</h1>{table_html}<p align=\"center\"></p></html>"
+    #<a href=formatting/David ><button class=grey style=\"height:75px;width:150px\">David's Resutls</button></a></p><p align=\"center\"><a href=formatting/Robert ><button class=grey style=\"height:75px;width:150px\">Robert's Resutls</button></a></p></body></html>"
+
+    return str(Markup(css + table_html))
 
 if __name__ == "__main__":
     app.debug = True
